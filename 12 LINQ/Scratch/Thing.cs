cfd09850197380d2a94e1d3711158db485113220
QACS-TL <114167055+QACS-TL@users.noreply.github.com>
2025-02-17 16:20:03 +0000

    Add files via upload

---
+++ 12 LINQ/Scratch/Thing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scratch
+{
+    internal class Thing
+    {
+        public string Name { get; set; }
+        public IEnumerable<Person> Persons { get; set; }
+    }
+}
