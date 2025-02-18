7bc34a3a31c06b99fb58472f67125e8d2aa4933b
QACS-TL <114167055+QACS-TL@users.noreply.github.com>
2025-02-18 16:22:40 +0000

    Add files via upload

---
+++ BankingLibrary/BankingLibrary/Account.cs
@@ -0,0 +1,45 @@
+﻿namespace BankingLibrary
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        private string accountNumber;
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+            set {
+                if (value.Length != 8) {
+                    throw new ArgumentException("Account number must be 8 chars.");
+                }
+                accountNumber = value; 
+            }
+        }
+        private decimal balance;
+        public decimal Balance
+        {
+            get => balance;
+            set
+            {
+                if (value < 0)
+                {
+                    value = 0;
+                }
+                balance = value;
+            }
+        }
+
+        public decimal Credit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+
+        public decimal Debit(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+
+
+    }
+}
---
+++ BankingLibrary/BankingUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using BankingLibrary;
+
+Account acc1 = new Account();
+Account acc2 = new Account();
+
+acc1.Name = "Daisy Goodwin";
+acc1.AccountNumber = "12345678";
+acc1.Balance = 0.00m;
+
+acc2.Name = "Sadia Saleem";
+acc2.AccountNumber = "23232323";
+acc2.Balance = 100.00m;
+
+Console.WriteLine($"Account: {acc1.AccountNumber}, Name {acc1.Name}, Balance: {acc1.Balance:C}");
+Console.WriteLine($"Account: {acc2.AccountNumber}, Name {acc2.Name}, Balance: {acc2.Balance:C}");
+
+acc1.Credit(100.00m);
+acc2.Debit(20.00M);
+
+Console.WriteLine($"Account: {acc1.AccountNumber}, Name {acc1.Name}, Balance: {acc1.Balance:C}");
+Console.WriteLine($"Account: {acc2.AccountNumber}, Name {acc2.Name}, Balance: {acc2.Balance:C}");
+
+acc2.Debit(90.00M);
+Console.WriteLine($"Account: {acc2.AccountNumber}, Name {acc2.Name}, Balance: {acc2.Balance:C}");
+
+try
+{
+    Account acc3 = new Account();
+    acc3.Name = "Tom Good";
+    acc3.AccountNumber = "123456789";
+    acc3.Balance = 0.00m;
+} catch(Exception ex) { Console.WriteLine(ex.Message);  }
+
+
+
