34a497d08e97f037ab555624eae8dd7f358d39b2
QACS-TL <114167055+QACS-TL@users.noreply.github.com>
2025-02-18 14:35:06 +0000

    Add files via upload

---
+++ ZooTzar/ZooTzarLibrary/Animal.cs
@@ -0,0 +1,30 @@
+﻿namespace ZooTzarLibrary
+{
+    public class Animal
+    {
+        public string name;
+        public int age;
+        public int limbCount;
+        private int health;
+
+        public string Eat(string food)
+        {
+            return $"I'm an animal called {this.name} using some of my {this.limbCount} limbs to eat {food}.";
+        }
+
+        public string Move(string direction, int distance)
+        {
+            return $"I'm an animal called {this.name} using some of my {this.limbCount} limbs to move {direction} for {distance} metres.";
+        }
+
+        public string Move(string direction)
+        {
+            return $"I'm an animal called {this.name} using some of my {this.limbCount} limbs to move {direction}.";
+        }
+
+        public string Move(int distance)
+        {
+            return $"I'm an animal called {this.name} using some of my {this.limbCount} limbs to move {distance} metres.";
+        }
+    }
+}
---
+++ ZooTzar/ZooTzarUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using ZooTzarLibrary;
+
+Animal animal = new Animal();
+animal.name = "Bonzo";
+animal.age = 2;
+animal.limbCount = 4;
+
+Animal animal1 = new Animal();
+animal1.name = "Fifi";
+animal1.age = 3;
+animal1.limbCount = 6;
+
+Console.WriteLine($"I'm an animal called {animal.name}. I am {animal.age} years old and I have {animal.limbCount} limbs.");
+Console.WriteLine($"I'm an animal called {animal1.name}. I am {animal1.age} years old and I have {animal1.limbCount} limbs.");
+
+Console.WriteLine(animal.Eat("Biscuits"));
+Console.WriteLine(animal.Move(8));
+Console.WriteLine(animal1.Move("North", 10));
+Console.WriteLine(animal1.Eat("Cheese"));
